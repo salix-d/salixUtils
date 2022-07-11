e42269d1a433ca68d89b6300fdb2dc746f6995f6
Salix <31168746+salix-d@users.noreply.github.com>
2022-07-11 17:45:04 -0400

    Update helpers.R

--- R/helpers.R
+++ R/helpers.R
@@ -3,14 +3,13 @@ v.chr     <- function(x, fun, ...) vapply(X = x, FUN = fun, FUN.VALUE = characte
 v.int     <- function(x, fun, ...) vapply(X = x, FUN = fun, FUN.VALUE = integer(1), ...)
 v.dbl     <- function(x, fun, ...) vapply(X = x, FUN = fun, FUN.VALUE = numeric(1), ...)
 v.lgl     <- function(x, fun, ...) vapply(X = x, FUN = fun, FUN.VALUE = logical(1), ...)
-v.lst     <- function(x, fun, ...) vapply(X = x, FUN = fun, FUN.VALUE = list(), ...)
-v.null    <- function(x) vapply(X = x, FUN = is.null, FUN.VALUE = logical(1))
+v.lst     <- function(x, fun, ...) vapply(X = x, FUN = fun, FUN.VALUE = vector("list", 1), ...)
 
 # operators
 `%ni%` <- Negate('%in%')
 
 # not empty
-is.empty     <- function(x) length(x) == 0
+is.empty     <- function(x) lengths(x) == 0
 is.empty.chr <- function(x) identical(x, character(0))
 is.empty.num <- function(x) identical(x, numeric(0))
 is.empty.int <- function(x) identical(x, integer(0))
