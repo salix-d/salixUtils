8ffd7063cc01c64cd641e81db53f3d67619d3da9
Salix <31168746+salix-d@users.noreply.github.com>
2022-07-11 16:53:21 -0400

    Update insertColumn.R

--- R/insertColumn.R
+++ R/insertColumn.R
@@ -4,8 +4,9 @@
 #' @param data      Vector, matrix, array or data.frame. Default is NA.
 #'
 #' @export
-insertColumns <- insertCol <- function(x, ind, col.names = ncol(df) + seq_along(colIndex), data = NA){
-  out <- x
-  out[, col.names] <- data
-  out[, order(c(col(x)[1,], ind))]
+insertCol <- function(x, ind, col.names = NULL, data = NA){
+  if(is.null(col.names)) col.names <- ncol(x) + seq_along(ind)
+  col.order <- order(c(col(x)[1,], ind))
+  x[,col.names] <- data
+  x[,col.order]
 }
