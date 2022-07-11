4c3bbe744bb8da000563953f8e7ce66706564d3a
Salix <31168746+salix-d@users.noreply.github.com>
2022-07-11 16:52:10 -0400

    add helper functions

--- R/classification2taxonomyTable.R
+++ R/classification2taxonomyTable.R
@@ -1,17 +1,51 @@
 #' @export
-classification2taxonomyTable <- class2taxo <- function(li,
-                                                       ranks = c("kingdom", "phylum", "class", "order", "family", "genus", "species"),
-                                                       rnames = T){
-  if(class(li) != "classification") stop("Wrong input class. Object's class must be 'classification' (from taxize)")
+class2taxo <- function(obj,
+                       ranks = c("kingdom", "phylum", "class", "order", "family", "genus", "species"),
+                       USE.NAMES = T){
   l <- length(ranks)
-  out <- vapply(li, function(x){
+  out <- vapply(obj, function(x){
     r <- match(ranks, x$rank, 0L)
     out <- array(NA_character_, dim = l, dimnames = list(ranks))
     out[x$rank[r]] <- x$name[r]
     out
-  }, character(l), USE.NAMES = rnames)
+  }, character(l), USE.NAMES = USE.NAMES)
   if(l==1) as.data.frame(out)
   else {
     as.data.frame(t(out))
   }
+}
+
+as.classification <- function(obj){
+  if(!is(obj, "classification")){
+    check <- function(o) length(d <- dim(o)) && all(d>0) && all(c("rank", "name") %in% colnames(o))
+    if("list" %in% class(obj)){
+      test <- vapply(obj, function(x){
+        if(check(x)) if(!"id" %in% colnames(x)) o[,"id"] <- NA
+        o[,c("rank", "name", "id")]
+      }, NA)
+      test <- any(test)
+    } else {
+      test <- check(obj)
+      if(test){
+        if(!"id" %in% colnames(obj)) obj[,"id"] <- NA
+        obj <- `names<-`(list(obj[,c("rank", "name", "id")]), obj$name[[length(obj$name)]])
+
+      }
+    }
+    if(!test) stop("Wrong input class. Object's class must be 'classification' (from taxize) or built similarly")
+    class(obj) <- "classification"
+  }
+  obj
+}
+is.classification <- function(obj){
+  check <- function(o) length(d <- dim(o)) && all(d>0) && all(c("rank", "name") %in% colnames(o))
+  test <- is(obj, "classification")
+  if(!test){
+    if("list" %in% class(obj)){
+      test <- any(vapply(obj, check, NA))
+    } else {
+      test <- check(obj)
+    }
+  }
+  test
 }
