43b1f380de6251fda1b1674f10f0f0c7e5e564ce
Salix <31168746+salix-d@users.noreply.github.com>
2024-03-11 00:58:56 -0400

    add export

--- R/messages.R
+++ R/messages.R
@@ -1,12 +1,9 @@
-
-warn <- function(..., call. = TRUE, caller = NULL) {
-  if (call. && missing(caller)) caller <- rlang::caller_call()
-}
+#' @export
 throw_error <- function(..., call. = TRUE, call.pos = 1L, class = ""){
   if (call.) {
     caller <- if (identical(parent.frame(), .GlobalEnv)) rlang::current_call() else rlang::caller_call(call.pos)
   }
-  if(length(caller)) {
+  if (length(caller)) {
     caller <- as.character(caller[vapply(caller, is.name, logical(1))])
     caller <- paste0("\033[31;1m\033]8;;ide:help:", caller, "\a", caller, "\033]8;;\a\033[0m")
     head <- paste0("\033[31;1mError\033[m", " in `", caller, "\033[31m`:\033[0m")
@@ -19,4 +16,7 @@ throw_error <- function(..., call. = TRUE, call.pos = 1L, class = ""){
   stop()
 }
 
+warn <- function(..., call. = TRUE, caller = NULL) {
+  if (call. && missing(caller)) caller <- rlang::caller_call()
+}
 # x <- function() throw_error("no arguments provided")
